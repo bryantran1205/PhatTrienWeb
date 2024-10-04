50f8f2733470410ed68c38ad2e2aba6d59c54d09
Trần Thanh Duy <DUY.TT22714@sinhvien.hsu.edu.vn>
2024-10-04 13:15:05 +0700

    Add project files.

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using System.Diagnostics;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Title"] = "Privacy Policy";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
