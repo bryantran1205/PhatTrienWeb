00159db9edcb78c2e38ce39f0eec0b0c35c90935
Trần Thanh Duy <duytranthanh1205@gmail.com>
2024-11-01 13:26:44 +0700

    update size ảnh , add view Detail,add Controller Product

---
+++ Project/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.AppData;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDBContext _db;
+        public ProductController(AppDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            Product p = _db.Products.Find(id);
+            List<Product> Lispro = _db.Products.ToList();
+            ViewBag.Lispro = Lispro;
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return View(p);
+        }
+    }
+}
